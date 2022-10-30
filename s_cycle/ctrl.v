bc1dfafeea0ff520a24868487836bbec79d3cddc
dcr <1456882404@qq.com>
2022-10-30 15:25:24 +0800

    initial project

---
+++ s_cycle/ctrl.v
@@ -0,0 +1,7 @@
+module ctrl(
+    output reg_write, 
+    output [3:0] aluop, 
+    input [5:0] op, 
+    input funct);
+
+endmodule
---
+++ s_cycle/single_cycle.v
@@ -0,0 +1,12 @@
+module single_cycle(clock, reset);
+
+input clock;
+input  reset;
+
+wire [31:0] pc;
+wire [31:0] npc;
+
+
+pc PC(.pc(pc), .clock(clock), .reset(reset), .npc(npc));
+
+endmodule
